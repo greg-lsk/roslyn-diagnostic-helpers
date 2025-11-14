463929a441931aebf886621532290179d92221f4
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-14 13:36:32 +0200

    fix:: removed nameof(..) keyword because caused a test to fail

--- RoslynDiagnosticHelpers/LocalizableResource/LocalizableStringHelper.cs
+++ RoslynDiagnosticHelpers/LocalizableResource/LocalizableStringHelper.cs
@@ -8,11 +8,11 @@ public static class LocalizableStringHelper
 {
     public static LocalizableString From<TResource>(string resourceName, ResourceManager resourceManager)
     {
-        return new LocalizableResourceString(nameof(resourceName), resourceManager, typeof(TResource));
+        return new LocalizableResourceString(resourceName, resourceManager, typeof(TResource));
     }
 
     public static LocalizableString From<TResource>(string resourceName, ResourceManager resourceManager, params string[] formatArguments)
     {
-        return new LocalizableResourceString(nameof(resourceName), resourceManager, typeof(TResource), formatArguments);
+        return new LocalizableResourceString(resourceName, resourceManager, typeof(TResource), formatArguments);
     }
 }
